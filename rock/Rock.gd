f578d1a03dbed23f3dfc449109714e33ced0707d
Vincent Marger <vincent.marger@gmail.com>
2024-10-14 11:26:33 -0400

    Add rock explosion animation on hit

--- rock/Rock.gd
+++ rock/Rock.gd
@@ -5,6 +5,7 @@ var size
 var radius
 var scale_factor = 0.2
 
+signal exploded
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,6 +16,7 @@ func start(_position, _velocity, _size):
 	position = _position
 	size = _size
 	mass = 1.5 * size
+	$Explosion.scale = Vector2.ONE * 0.75 * size
 
 	$Sprite2D.scale = Vector2.ONE * scale_factor * size
 	var shape = CircleShape2D.new()
@@ -31,6 +33,17 @@ func _integrate_forces(physics_state):
 	xform.origin.y = wrapf(xform.origin.y, 0 - radius, screensize.y + radius)
 	physics_state.transform = xform
 
+
+func explode():
+	$CollisionShape2D.set_deferred("disabled", true)
+	$Sprite2D.hide()
+	$Explosion/AnimationPlayer.play("explosion")
+	$Explosion.show()
+	exploded.emit(size, radius, position, linear_velocity)
+	linear_velocity = Vector2.ZERO
+	angular_velocity = 0
+	await $Explosion/AnimationPlayer.animation_finished
+	queue_free()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):

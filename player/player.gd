892f8156e2ca35567c41738aa6f42fe81f374f02
Vincent Marger <vincent.marger@gmail.com>
2024-10-14 15:51:37 -0400

    Add ship/rock collision and gameover

--- player/player.gd
+++ player/player.gd
@@ -24,14 +24,24 @@ func change_state(new_state):
 	match new_state:
 		INIT:
 			$CollisionShape2D.set_deferred("disabled", true)
+			$Sprite2D.modulate.a = 0.5
 		ALIVE:
+			print("test")
 			$CollisionShape2D.set_deferred("disabled", false)
+			$Sprite2D.modulate.a = 1
 		INVULNERABLE:
 			$CollisionShape2D.set_deferred("disabled", true)
+			$Sprite2D.modulate.a = 0.5
+			$InvulnerabilityTimer.start()
 		DEAD:
 			$CollisionShape2D.set_deferred("disabled", true)
+			$Sprite2D.hide()
+			linear_velocity = Vector2.ZERO
+			dead.emit()
 	state = new_state
 
+func _on_invulnerability_timer_timeout():
+	change_state(ALIVE)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -99,3 +109,16 @@ func reset():
 	$Sprite2D.show()
 	lives = 3
 	change_state(ALIVE)
+
+func _on_body_entered(body):
+	if body.is_in_group("rocks"):
+		body.explode()
+		lives -= 1
+		explode()
+
+func explode():
+	$Explosion.show()
+	$Explosion/AnimationPlayer.play("explosion")
+	await $Explosion/AnimationPlayer.animation_finished
+	$Explosion.hide()
+
